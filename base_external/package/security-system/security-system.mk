37c6251b63dfe343cbdb876fa1a24ebdba6f68f1
Zehariah Oginsky <zeog1488@colorado.edu>
2024-04-28 11:20:40 -0600

    Final commit

--- base_external/package/security-system/security-system.mk
+++ base_external/package/security-system/security-system.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-SECURITY_SYSTEM_VERSION = 0168ce5dadf45a94679014452bff43c2f597f501
+SECURITY_SYSTEM_VERSION = 2a4946a43fa48fb1121e462f88f78fbbda119f31
 SECURITY_SYSTEM_SITE = git@github.com:zeog1488/final-project-source-zeog1488.git
 SECURITY_SYSTEM_SITE_METHOD = git
 SECURITY_SYSTEM_GIT_SUBMODULES = YES
@@ -16,6 +16,7 @@ endef
 
 define SECURITY_SYSTEM_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/server/securitySystem $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/securitySystem-start-stop.sh $(TARGET_DIR)/etc/init.d/S101securitySystem
 endef
 
 $(eval $(generic-package))
